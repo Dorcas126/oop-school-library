5688f6d76e380760f83e009bcc35c8565aaa3415
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-04 18:06:44 +0200

    Correct linters errors

--- person.rb
+++ person.rb
@@ -1,25 +1,23 @@
-class person
-    def initialize(id,age,name:'unknown',parent_permission:true)
+class Person
+  def initialize(id, age, name: 'unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-        @id = id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  attr_reader :id
+  attr_accessor :name, :age
 
-    attr_reader :id
-    attr_accessor :name, :age        
+  private
 
-    private
+  def of_age?
+    @age >= 18
+  end
 
-    def of_age?
-        @age >= 18
-    end
-    
-    public
+  public
 
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
-
