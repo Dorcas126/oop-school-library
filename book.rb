d925a8d407da6441436afb60201f58099193a9e6
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-07 22:57:42 +0200

    add basic ui

---
+++ book.rb
@@ -0,0 +1,16 @@
+class Book
+    attr_accessor :title, :author, :id
+    attr_reader :rentals
+  
+    def initialize(title, author)
+      @id = Random.rand(1..1000)
+      @title = title
+      @author = author
+      @rentals = []
+    end
+  
+    def add_rental(date, person)
+      Rental.new(date, self, person)
+    end
+  end
+  
--- person.rb
+++ person.rb
@@ -1,28 +1,33 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @age = age
+    @id = Random.rand(1..1000)
     @name = name
+    @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
-  attr_accessor :age, :name
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-  def correct_name
-    @name
+  def add_rental(date, book)
+    rentals << Rental.new(date, book, self)
+  end
+
+  def to_s
+    "[Person] Name: #{name}, ID: #{id}, Age: #{age}"
   end
 
   private
 
   def of_age?
     @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
   end
 end
